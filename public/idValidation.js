e065f8bf93557f34dcb349162fb0e36449bffe6b
Jomari Borines <jomariborines55@gmail.com>
2024-09-17 21:56:15 +0800

    UPDATE

--- public/idValidation.js
+++ public/idValidation.js
@@ -24,37 +24,36 @@ const config = {
 };
 
 function startScanning() {
-  let data;
   html5QrCode
     .start(
       { facingMode: "environment" },
       config,
       async (decodedText, decodedResult) => {
-        console.log(`Code matched = ${decodedText}`, decodedResult);
+        try {
+          console.log(`Code matched = ${decodedText}`, decodedResult);
+          detectionStatus.classList.remove("hidden");
 
-        detectionStatus.classList.remove("hidden");
+          const response = await fetch("/app/id-validation/submit", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({ studentNumber: decodedText }),
+          });
+          const data = await response.json();
+          studentNumber.textContent = data.data.validatedStudent.studentNumber;
 
-        const response = await fetch("/app/id-validation/submit", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({ studentNumber: decodedText }),
-        });
-        data = await response.json();
-
-        html5QrCode
-          .stop()
-          .then(() => {
+          html5QrCode.stop().then(() => {
             setTimeout(() => {
               startScanning();
             }, 5000);
-          })
-          .catch((err) => console.error(`Failed to stop scanning: ${err}`));
+          });
+        } catch (err) {
+          console.error(err);
+        }
       },
       (error) => {
-        // detectionStatus.classList.add("hidden");
-        studentNumber.textContent = data.data.validatedStudent.studentNumber;
+        detectionStatus.classList.add("hidden");
       }
     )
     .catch((err) => {
