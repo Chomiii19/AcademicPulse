5e07fdf395182349ece1fb38bf5642aa7bace0eb
Jomari Borines <jomariborines55@gmail.com>
2024-09-18 21:49:59 +0800

    UPDATE

--- public/dashboard.js
+++ public/dashboard.js
@@ -23,17 +23,18 @@ const idValidated = async () => {
     "December",
   ];
 
-  const data = monthsList.reduce((acc, month) => {
+  const dataList = monthsList.reduce((acc, month) => {
     acc[month] = 0;
     return acc;
   }, {});
 
+  console.log(dataList);
   dataAPI.data.forEach((log) => {
-    data[months[log.month - 1]] = log.count;
+    dataList[months[log.month - 1]] = log.count;
   });
 
-  const months = Object.keys(data);
-  const counts = Object.values(data);
+  const months = Object.keys(dataList);
+  const counts = Object.values(dataList);
 
   const idValidatedGraph = new Chart(ctx, {
     type: "bar",
