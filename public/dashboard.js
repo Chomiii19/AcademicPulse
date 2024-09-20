34c8a51fbd82c4f836a1c34a6b1540f73c876422
Jomari Borines <jomariborines55@gmail.com>
2024-09-20 22:52:47 +0800

    UPDATE

--- public/dashboard.js
+++ public/dashboard.js
@@ -105,7 +105,7 @@ const idValidated = async (
       acc[list] = 0;
       return acc;
     }, {});
-    console.log(url, monthLists, type);
+
     if (type === "year") {
       dataAPI.data.forEach((log) => {
         dataList[lists[log.month - 1]] = log.count;
@@ -144,6 +144,9 @@ const idValidated = async (
         animation: {
           duration: 1000,
           easing: "easeOutBounce",
+          onComplete: () => {
+            console.log("Animation complete!");
+          },
         },
       },
     });
