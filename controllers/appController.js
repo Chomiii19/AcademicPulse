6169461eed8dfce96fc16bfab4cf288949d815b2
Jomari Borines <jomariborines55@gmail.com>
2024-09-22 23:41:20 +0800

    UPDATE

--- controllers/appController.js
+++ controllers/appController.js
@@ -5,8 +5,9 @@ import AppError from "../utils/appError.js";
 import StudentLog from "../models/student-log.js";
 
 const date = new Date();
-const timezoneOffset = new Date().getTimezoneOffset() / 60;
-const utc = -8;
+// const timezoneOffset = new Date().getTimezoneOffset() / 60;
+const timezoneOffset = -8;
+const utc = 8;
 
 const utcDate = () => {
   const today = new Date().toISOString().split("T")[0];
