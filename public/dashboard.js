8fb6289a5e0888bbd38f0b6731aaaf60dcda46e0
Jomari Borines <jomariborines55@gmail.com>
2024-09-25 14:10:24 +0800

    UPDATE

--- public/dashboard.js
+++ public/dashboard.js
@@ -217,7 +217,7 @@ const doughnutGraph = async () => {
       options: {
         cutout: "90%",
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
       },
     });
   } catch (err) {
